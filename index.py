90701ac3f27b50fb4656dde7747fc2957f05da0c
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 13:20:53 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
 newstring = "Coronavirus is a serious pandemic!"
 print(newstring.index("o")) #this prints out 1 because the location of the first occurence of "o" is
-#1 character away from the 1st character. Notice only the 1st o is considered. 
+#1 character away from the 1st character. Notice only the 1st o is considered. However, if you used .count("o") it would 
+#print 3 because there are 3 o's in this string
