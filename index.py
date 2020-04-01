2b4cb99c47b8caaf3ca12caadc5809e31b23b930
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 13:14:33 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,3 @@
+newstring = "Coronavirus is a serious pandemic!"
+print(newstring.index("o")) #this prints out 1 because the location of the first occurence of "o" is
+#1 character away from the 1st character. Notice only the 1st o is considered. 
